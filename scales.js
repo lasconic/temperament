cea7f84ca8a014d1af8b24a0f1022e94c10617ee
Joachim Schmitz <jojo@schmitz-digital.de>
2012-06-03 14:57:08 +0200

    made fit for MuseScore 2.0

--- scales.js
+++ scales.js
@@ -5,7 +5,7 @@
 //  " S C A L E S " plugin
 //
 //	Manages and applies score temperaments.
-//	Version 0.4 - Date 01.06.2012
+//	Version 0.5 - Date 03.06.2012
 //
 //	By Maurizio M. Gavioli, 2010.
 //	Derived from a plugin by lasconic.
